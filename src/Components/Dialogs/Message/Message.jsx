7af5276d580e120fdedcb3a817b3deb684d5968c
Silv228 <101052646+Silv228@users.noreply.github.com>
2023-04-30 10:47:43 +0000

    new branch

--- src/Components/Dialogs/Message/Message.jsx
+++
@@ -1,8 +0,0 @@
-import style from "./Message.module.css";
-const Message = ({children, state}) => {
-    return(
-        <div className={`${style.message}  ${state === 'to'? style.to : style.from}`}>{children}</div>
-    )
-}
-
-export default Message;
--- src/Components/common/Validate/validators.js
+++
@@ -1,8 +0,0 @@
-export const required = (value) => {
-    if (!value) return "REQUIRED FIELD"
-    return undefined
-}
-export const validEmail = (value) => {
-    if ((/^[A-Z\d.-]+@[A-Z\d]+(\.[A-Z]{2,})$/i).test(value)) return undefined
-    return "INVALID EMAIL"
-}
